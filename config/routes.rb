bcb597e431784b8c0bb3d962e7b801ed911c3a17
Tomoka18 <gudegude18@gmail.com>
2021-06-09 14:35:49 +0000

    [Update]startリンク実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'books/top'
+  get 'homes/top'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  get 'top/index'
-  get 'top/show'
-  get 'top/edit'
-  root to: 'books#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+
 end
