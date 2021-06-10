2aae135d8fc3ab72e336d6d626ae9ae70d6bc6f9
Tomoka18 <gudegude18@gmail.com>
2021-06-10 14:46:20 +0000

    [Update]show追加途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,28 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new
+    @book = Book.new
+
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
+  end
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+
+private
+def book_params
+  params.permit(:title,:body)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get 'books/index'
   get 'books/show'
   get 'books/edit'
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show',as:'book'
+  resources :books
   root :to => 'homes#top'
 
-end
+  end
