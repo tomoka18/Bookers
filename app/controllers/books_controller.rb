d5a6fd209d58a0aeaa68130781434995bb4c35eb
Tomoka18 <gudegude18@gmail.com>
2021-06-12 05:29:36 +0000

    [Update３−８]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def new
@@ -14,7 +15,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -22,12 +23,24 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
 private
 def book_params
-  params.permit(:title,:body)
+  params.require(:book).permit(:title,:body)
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show',as:'book'
+  get 'books/:id/edit' => 'books#edit',as:'edit_book'
+  patch 'books/:id' => 'books#update',as:'update_book'
+  delete 'books/:id' => 'books#destroy',as:'destroy_book'
   resources :books
   root :to => 'homes#top'
 
